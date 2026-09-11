764c42e62d97e1cd6b1db15836d4a5576707a4b5
Anton <anteotalks@gmail.com>
2026-09-11 20:34:10 +0300

    fix: deploy to root of gh-pages

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     define: publicVars,
   },
   output: {
-    assetPrefix: '/russia-interactive-map.github.io/',
+    assetPrefix: '/',
   },
   server: {
     cors: {
